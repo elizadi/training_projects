71cab94169891f4fbfe6b760992414d2b3ef8c7f
elizadi <47442848+elizadi@users.noreply.github.com>
2022-04-11 19:35:38 +0300

    Add files via upload
    Factorial finder using both loops and recursion

---
+++ factorial_finder.py
@@ -0,0 +1,12 @@
+def factorial(num, res, count):
+    if num <= 1:
+        return 1
+    while count <= num:
+        return factorial(num, res * count, count + 1)
+    else:
+        return res
+
+r = 1
+c = 1
+n = int(input('Print number: '))
+print('Factorial %d = ' %n, factorial(n, r, c))
